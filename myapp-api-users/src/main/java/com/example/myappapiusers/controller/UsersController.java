7889604afb7c5142e6e469ff9be54ef4c77f54dc
yoonbyeong chae <dbsqud1105@gmail.com>
2020-05-12 15:30:46 +0900

    first commit

---
+++ myapp-api-users/src/main/java/com/example/myappapiusers/controller/UsersController.java
@@ -0,0 +1,20 @@
+package com.example.myappapiusers.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    Environment env;
+
+    @GetMapping("/status/check")
+    public String status(){
+         return String.format("Working on port %s", env.getProperty("local.server.port"));
+    }
+}
