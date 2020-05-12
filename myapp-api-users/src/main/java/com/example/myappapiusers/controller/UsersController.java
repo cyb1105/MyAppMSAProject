ba03060554da636a7bba8eec14de29a5177a66ea
yoonbyeong chae <dbsqud1105@gmail.com>
2020-05-12 17:32:52 +0900

    myapp

--- myapp-api-users/src/main/java/com/example/myappapiusers/controller/UsersController.java
+++ myapp-api-users/src/main/java/com/example/myappapiusers/controller/UsersController.java
@@ -1,10 +1,12 @@
 package com.example.myappapiusers.controller;
 
+import com.example.myappapiusers.data.UserEntity;
+import com.example.myappapiusers.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -13,8 +15,21 @@ public class UsersController {
     @Autowired
     Environment env;
 
+    @Autowired
+    private UserRepository repository;
+
     @GetMapping("/status/check")
     public String status(){
          return String.format("Working on port %s", env.getProperty("local.server.port"));
     }
+
+    //사용자 추가 API
+
+    @PostMapping("/user")
+    public List<UserEntity> createUser(@RequestBody UserEntity userEntity){
+        repository.save(userEntity);
+        return repository.findAll();
+    }
+
+
 }
